821dba2f59135586a6c2f6876116b9122254cf63
ZIF369 <58552006+ZIF369@users.noreply.github.com>
2020-01-12 01:55:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,5 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
+# Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config_generate
