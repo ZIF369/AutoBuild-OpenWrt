bfaec4bc67f442afae451b892491af0d7afe9e8a
ZIF369 <58552006+ZIF369@users.noreply.github.com>
2020-02-02 17:10:14 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,8 +2,9 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# Author: ZIF
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.10/g' openwrt/package/base-files/files/bin/config_generate
+# Modify KERNEL PATCHVER
+sed -i 's/4.19/4.14/g' openwrt/target/linux/x86/Makefile
