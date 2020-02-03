fc7257e1bed14e9313a6f1eacc505e73cd2513bd
ZIF369 <58552006+ZIF369@users.noreply.github.com>
2020-02-03 19:02:23 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,3 +6,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.10/g' openwrt/package/base-files/files/bin/config_generate
+# Modify OpenWrt version
+sed -i 's/4.19/4.14/g' openwrt/target/linux/x86/Makefile
