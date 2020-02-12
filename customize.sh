04a268674dd049542fba8b3729577dd3e70217e1
ZIF369 <58552006+ZIF369@users.noreply.github.com>
2020-02-12 15:02:56 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.10/g' openwrt/package/base-files/files/bin/config_generate
-# Modify Kernel Version
-sed -i 's/4.19/4.14/g' openwrt/target/linux/x86/Makefile
 # Modify Firmware Version
 sed -i 's/<%=pcdata(ver.distname)%> <%=pcdata(ver.distversion)%>/<%=pcdata(ver.distname)%> <%=pcdata(ver.distversion)%> <%:compiled by ZIF%>/g' openwrt/package/lean/autocore/files/index.htm
